c7279922c1f71b2a2a7f3613de22736ca61381d3
Ibrahim Abdulraheem <opeibrahim@gmail.com>
2021-02-22 19:18:16 +0100

    Final Commit

--- index.php
+++ index.php
@@ -5,8 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task 2</title>
+    <style>
+    body{
+        color: blue;
+    }
+    h1{
+        color: black;
+    }
+    </style>
 </head>
 <body>
+<h1>Generated Recharge Pins</h1>
 <?php
 $digits = 16;
 $count = 0;
